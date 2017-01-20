6897f9fea05129ae712dbe6223d04ad10a1a9c73
viviandsilva <viviandsilva@gatech.edu>
2017-01-20 18:52:36 -0500

    Added-link mechanism

--- app/src/main/java/com/example/vivian/capstone_test/LoginActivity.java
+++ app/src/main/java/com/example/vivian/capstone_test/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.vivian.capstone_test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, EventsActivity.class));
+            }
+        });
     }
 }
