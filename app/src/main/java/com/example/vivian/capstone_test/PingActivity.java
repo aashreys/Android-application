d59396864700bb3bf3d100022183955e47523b39
aashreys <aashrey9sharma@gmail.com>
2017-01-22 22:28:14 +0530

    Linked buttons in button bar to activities.

--- app/src/main/java/com/example/vivian/capstone_test/PingActivity.java
+++ app/src/main/java/com/example/vivian/capstone_test/PingActivity.java
@@ -9,5 +9,6 @@ public class PingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ping);
+        Utils.configureButtonBar(this);
     }
 }
