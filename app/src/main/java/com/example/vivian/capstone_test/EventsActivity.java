43f55f2d9ee2c1a94d688018c9a8e9482971736e
viviandsilva <viviandsilva@gatech.edu>
2017-01-24 00:14:44 -0500

    Added getAll(), trying to figure out using RecyclerView

--- app/src/main/java/com/example/vivian/capstone_test/EventsActivity.java
+++ app/src/main/java/com/example/vivian/capstone_test/EventsActivity.java
@@ -3,6 +3,8 @@ package com.example.vivian.capstone_test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.vivian.capstone_test.persistence.event.EventRepository;
+
 public class EventsActivity extends AppCompatActivity {
 
     @Override
@@ -10,5 +12,7 @@ public class EventsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
         Utils.configureButtonBar(this);
+        EventRepository thisEvent = new EventRepository();
+        thisEvent.getAll();
     }
 }
